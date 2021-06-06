f19829110e9c93046cc433e0c69e7802648f2825
Truong Nguyen <nguyenvantruong170797@gmail.com>
2021-06-06 22:35:35 +0700

    feature(setup-testing): setup testing

---
+++ jest.config.unit.js
@@ -0,0 +1,33 @@
+module.exports = {
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.test.json',
+      babelConfig: {},
+    },
+  },
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.ts?$': 'ts-jest',
+  },
+  moduleNameMapper: {},
+  testRegex: '\\.test\\.ts?$',
+  setupFiles: [],
+  setupFilesAfterEnv: [],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '**/*.ts',
+    '!**/src/**/*.test.ts',
+    '!**/src/error/logger.ts',
+    '!**/src/**/index.ts',
+  ],
+  coverageDirectory: 'coverage',
+  moduleDirectories: ['node_modules', 'src'],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+};
