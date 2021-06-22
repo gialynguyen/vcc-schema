44e1d8ddc01feda5cc4b201e80fb0fad79ab0748
Truong Nguyen <nguyenvantruong170797@gmail.com>
2021-06-23 00:07:11 +0700

    test(datatype): write a unit test for datatype

--- src/datatype/__tests__/mixed.test.ts
+++ src/datatype/__tests__/mixed.test.ts
@@ -1,11 +1,78 @@
-import { mixed } from '../';
+import { mixed, MixedType, string, StringType, number } from '../';
+import { ErrorSet, InvalidTypeError, InvalidFieldError } from '../../error';
 
 describe("DataType Mixed", () => {
-    const subject = mixed({});
+    const subject = mixed({
+        name: string(),
+        age: number(),
+    });
 
-    it('should have instance of AnyType', () => {
-        expect(1).toBe(1);
-        // expect(subject).toBeInstanceOf(AnyType);
-        // console.log('subject: ', subject);
-    })
+    it('should have instance of MixedType', () => {
+        const mixedValue = { name: 'Người Da Màu', age: 24 };
+        expect(subject).toBeInstanceOf(MixedType);
+        expect(subject.children['name']).toBeInstanceOf(StringType);
+        expect(subject.parser(mixedValue)).toEqual(mixedValue);
+        expect(subject.parser({ age: 24 }, { tryParser: true, paths: [] }).name).toBeUndefined();
+        expect(subject.parser({ age: 20 }, { deepTryParser: true, tryParser: true, paths: [] }).name).toBeUndefined();
+    });
+
+    describe('with errors', () => {
+        it('should throw an InvalidTypeError error', () => {
+            const subject = mixed({
+                name: string()
+            }, { error: "The value must be an object" });
+
+            try {
+                subject.parser(null);
+            } catch (err) {
+                expect(err).toBeInstanceOf(ErrorSet);
+                expect(err.errors[0]).toBeInstanceOf(InvalidTypeError);
+                expect(err.errors[0].error.message).toEqual('The value must be an object');
+            }
+        });
+
+        it('should throw an InvalidFieldError error', () => {
+            const subject = mixed({
+                name: string()
+            });
+
+            try {
+                subject.parser({ age: 24 });
+            } catch (err) {
+                expect(err.errors[0]).toBeInstanceOf(InvalidFieldError);
+            }
+        });
+    });
+
+    describe('pick', () => {
+        it('should have instance of MixedType', () => {
+            const pickSubject = subject.pick(['name']);
+            expect(pickSubject).toBeInstanceOf(MixedType);
+            expect(pickSubject.parser({ name: 'pick' })['name']).toEqual('pick');
+        })
+    });
+
+    describe('omit', () => {
+        it('should have instance of MixedType', () => {
+            const pickSubject = subject.omit(['name']);
+            expect(pickSubject).toBeInstanceOf(MixedType);
+            expect(pickSubject.parser({ age: 24 })['age']).toBe(24);
+        })
+    });
+
+    describe('strict', () => {
+        it('should have instance of MixedType', () => {
+            const strictSubject = subject.strict(false);
+            expect(strictSubject).toBeInstanceOf(MixedType);
+        })
+
+        it('should throw an InvalidFieldError error', () => {
+            const strictSubject = subject.strict(true);
+            try {
+                strictSubject.parser({ major: 'Software engineer' });
+            } catch (err) {
+                expect(err.errors[0]).toBeInstanceOf(InvalidFieldError);
+            }
+        });
+    });
 });
