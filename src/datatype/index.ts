42acad20fadd572e33d0dcf2c153499adf165391
gialynguyen <emthanchet@gmail.com>
2021-07-28 23:23:35 +0700

    feat(module): export default SchemaType

--- src/datatype/index.ts
+++ src/datatype/index.ts
@@ -1,3 +1,31 @@
+import { string } from "./string";
+import { number } from "./number";
+import { mixed } from "./mixed";
+import { oneOf } from "./oneOf";
+import { undefined } from "./undefined";
+import { nullable } from "./null";
+import { func } from "./func";
+import { boolean } from "./boolean";
+import { any } from "./any";
+import { unknown } from "./unknown";
+import { date } from "./date";
+import { array } from "./array";
+
+export const VCS = {
+  string,
+  number,
+  mixed,
+  oneOf,
+  undefined,
+  nullable,
+  func,
+  boolean,
+  any,
+  unknown,
+  date,
+  array,
+};
+
 export * from "./string";
 export * from "./number";
 export * from "./mixed";
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+import { VCS } from "./datatype";
+
+export default VCS;
+
 export * from "./@types";
 export * from "./datatype";
 export * from "./core";
